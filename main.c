4b5e0107a4534f327130fe5e62e681ef98ee19c9
yunusp <yunuspoonawala52@gmail.com>
2023-05-18 00:47:44 +0530

    Working model

--- main.c
+++ main.c
@@ -7,6 +7,7 @@ float train[][2] = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
 
 float rand_float() { return (float)rand() / (float)RAND_MAX; }
 
+// cost function. Square of errors.
 float cost(float w) {
     float result = 0.0f;
     for (size_t i = 0; i < train_count; ++i) {
@@ -20,9 +21,19 @@ float cost(float w) {
 }
 
 int main() {
-    // srand(time(0));
-    srand(69);
+    srand(time(0));
+    // srand(69);
+    const float eps = 1e-3;
+    const float rate = 1e-3;
 
     float w = rand_float() * 10;
-    printf("%f\n", cost(w));
+
+    for (size_t i = 0; i < 80000; i++) {
+        float dcost =
+            (cost(w + eps) - cost(w)) / eps;  // derivative limit formula
+        printf("%f\n", cost(w));
+        w -= rate * dcost;
+    }
+    puts("===================================");
+    printf("%f\n", w);
 }
