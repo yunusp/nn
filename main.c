277e8c213bb3df710276f1c0b94931ab1bfe2e88
yunusp <yunuspoonawala52@gmail.com>
2023-05-17 23:36:09 +0530

    cost function

--- main.c
+++ main.c
@@ -7,20 +7,22 @@ float train[][2] = {{0, 0}, {1, 2}, {2, 4}, {3, 6}, {4, 8}};
 
 float rand_float() { return (float)rand() / (float)RAND_MAX; }
 
-int main() {
-    // srand(time(0));
-    srand(69);
-
-    float w = rand_float() * 10;
-
+float cost(float w) {
     float result = 0.0f;
     for (size_t i = 0; i < train_count; ++i) {
         float x = train[i][0];
         float y = x * w;
         float d = y - train[i][1];
         result += d * d;
-        printf("Actual: %f, expected: %f\n", y, train[i][1]);
     }
     result /= train_count;
-    printf("%f\n", result);
+    return result;
+}
+
+int main() {
+    // srand(time(0));
+    srand(69);
+
+    float w = rand_float() * 10;
+    printf("%f\n", cost(w));
 }
