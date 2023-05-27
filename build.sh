747369b3c392a8f2ed144d810851520ccc617a10
yunusp <yunuspoonawala52@gmail.com>
2023-05-28 01:11:14 +0530

    Implement stubs + less dependence on libc

---
+++ build.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+set -xe
+clang -Wall -Wextra -o nn nn.c -lm
--- nn.h
+++ nn.h
@@ -1,6 +1,16 @@
 #include <stddef.h>
+#include <stdlib.h>
 #ifndef NN_H_
 #define NN_H_
+
+#ifndef NN_ASSERT
+#include <assert.h>
+#define NN_ASSERT assert
+#endif // NN_ASSERT
+
+#ifndef NN_MALLOC
+#define NN_MALLOC malloc
+#endif // NN_MALLOC
 
 typedef struct {
   size_t rows;
@@ -15,8 +25,31 @@ void mat_print(Mat a);
 
 #endif // NN_H_
 
+#define NN_IMPLEMENTATION
+// NOLINTBEGIN
 #ifdef NN_IMPLEMENTATION
 
-Mat mat_alloc()
+Mat mat_alloc(size_t rows, size_t cols) {
+  Mat m;
+  m.rows = rows;
+  m.cols = cols;
+  m.es = (float *)NN_MALLOC(sizeof(*m.es) * rows * cols);
+  assert(m.es != NULL);
+  return m;
+}
+
+void mat_dot(Mat dst, Mat a, Mat b) {
+  (void)dst;
+  (void)a;
+  (void)b;
+}
+
+void mat_sum(Mat dst, Mat a) {
+  (void)dst;
+  (void)a;
+}
+
+void mat_print(Mat a) { (void)a; }
 
 #endif // NN_IMPLEMENTATION
+// NOLINTEND
