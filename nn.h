7a018eaebb499b52fd99804169c87e96a1b895a4
yunusp <yunuspoonawala52@gmail.com>
2023-05-28 00:25:55 +0530

    Added stuff

--- nn.h
+++ nn.h
@@ -1,10 +1,22 @@
+#include <stddef.h>
 #ifndef NN_H_
 #define NN_H_
 
+typedef struct {
+  size_t rows;
+  size_t cols;
+  float *es;
+} Mat;
+
+Mat mat_alloc(size_t rows, size_t cols);
+void mat_dot(Mat dst, Mat a, Mat b);
+void mat_sum(Mat dst, Mat a);
+void mat_print(Mat a);
+
 #endif // NN_H_
 
-#define NN_IMPLEMENTATION
-
 #ifdef NN_IMPLEMENTATION
+
+Mat mat_alloc()
 
 #endif // NN_IMPLEMENTATION
