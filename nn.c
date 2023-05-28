cf8809045b4689027588e22cc1179a7a5cc172ec
yunusp <yunuspoonawala52@gmail.com>
2023-05-29 00:54:58 +0530

    Xor works :))

--- nn.c
+++ nn.c
@@ -86,6 +86,44 @@ void finite_difference(Xor m, Xor g, float eps, Mat ti, Mat to) {
   }
 }
 
+Xor xor_alloc(void) {
+  Xor m;
+  m.a0 = mat_alloc(1, 2);
+  m.w1 = mat_alloc(2, 2);
+  m.b1 = mat_alloc(1, 2);
+  m.a1 = mat_alloc(1, 2);
+  m.w2 = mat_alloc(2, 1);
+  m.b2 = mat_alloc(1, 1);
+  m.a2 = mat_alloc(1, 1);
+  return m;
+}
+
+void xor_learn(Xor m, Xor g, float rate) {
+  for (size_t i = 0; i < m.w1.rows; ++i) {
+    for (size_t j = 0; j < m.w1.cols; ++j) {
+      MAT_AT(m.w1, i, j) -= rate * MAT_AT(g.w1, i, j);
+    }
+  }
+
+  for (size_t i = 0; i < m.b1.rows; ++i) {
+    for (size_t j = 0; j < m.b1.cols; ++j) {
+      MAT_AT(m.b1, i, j) -= rate * MAT_AT(g.b1, i, j);
+    }
+  }
+
+  for (size_t i = 0; i < m.w2.rows; ++i) {
+    for (size_t j = 0; j < m.w2.cols; ++j) {
+      MAT_AT(m.w2, i, j) -= rate * MAT_AT(g.w2, i, j);
+    }
+  }
+
+  for (size_t i = 0; i < m.b2.rows; ++i) {
+    for (size_t j = 0; j < m.b2.cols; ++j) {
+      MAT_AT(m.b2, i, j) -= rate * MAT_AT(g.b2, i, j);
+    }
+  }
+}
+
 float td[] = {
     0, 0, 0,
 
@@ -114,17 +152,11 @@ int main() {
       .es = td + 2,
   };
 
-  MAT_PRINT(ti);
-  MAT_PRINT(to);
+  // MAT_PRINT(ti);
+  // MAT_PRINT(to);
 
-  Xor m;
-  m.a0 = mat_alloc(1, 2);
-  m.w1 = mat_alloc(2, 2);
-  m.b1 = mat_alloc(1, 2);
-  m.a1 = mat_alloc(1, 2);
-  m.w2 = mat_alloc(2, 1);
-  m.b2 = mat_alloc(1, 1);
-  m.a2 = mat_alloc(1, 1);
+  Xor m = xor_alloc();
+  Xor g = xor_alloc();
 
   mat_rand(m.w1, 0.0f, 1.0f);
   mat_rand(m.w2, 0.0f, 1.0f);
@@ -132,7 +164,17 @@ int main() {
   mat_rand(m.b2, 0.0f, 1.0f);
 
   printf("Cost: %f\n", cost(m, ti, to));
-#if 0
+
+  float eps = 1e-1;
+  float rate = 1e-1;
+
+  for (size_t i = 0; i < 100 * 1000; i++) {
+    finite_difference(m, g, eps, ti, to);
+    xor_learn(m, g, rate);
+  }
+  printf("Cost: %f\n", cost(m, ti, to));
+
+#if 1
   for (size_t i = 0; i < 2; ++i) {
     for (size_t j = 0; j < 2; ++j) {
 
